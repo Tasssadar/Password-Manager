cefa9ef5f033a1548327bfead46ff08c3da8f2d6
Frca <killerfrca@gmail.com>
2012-05-08 13:55:25 +0200

    [pm1] First commit
    Signed-off-by: Frca <killerfrca@gmail.com>

---
+++ src/com/frca/passmgr/SpravceHeselActivity.java
@@ -0,0 +1,14 @@
+package com.frca.passmgr;
+
+import com.frca.passmgr.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpravceHeselActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
